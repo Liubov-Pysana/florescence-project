fa6355b6e3900fd232a5180da79cd6e44e0d8636
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-01-23 15:24:14 +0200

    changes header burger

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,16 +1,13 @@
 const burgerBtn = document.querySelector('.burger-menu');
 const closeBtn = document.querySelector('.burger-close');
 const nav = document.querySelector('.header-navigation');
-const body = document.body;
 
 function openMenu() {
   nav.classList.add('active');
-  body.classList.add('is-hidden');
 }
 
 function closeMenu() {
   nav.classList.remove('active');
-  body.classList.remove('is-hidden');
 }
 
 burgerBtn.addEventListener('click', openMenu);
@@ -20,4 +17,23 @@ document.addEventListener('click', (event) => {
   if (!nav.contains(event.target) && !burgerBtn.contains(event.target)) {
     closeMenu();
   }
+});
+
+document.querySelectorAll('.header-navigation-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    if (!this.classList.contains('external-link')) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+
+      closeMenu();
+    } else {
+      closeMenu();
+    }
+  });
 });
